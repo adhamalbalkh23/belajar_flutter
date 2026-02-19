9b1a0d0685b8434e6aa273140b5a9fd37418a194
adhamalbalkh23 <adhamdermawan8@gmail.com>
2026-02-19 13:45:24 +0700

    New Tugas 5_Flutter

--- lib/day_10/tugas5.dart
+++ lib/day_10/tugas5.dart
@@ -31,16 +31,7 @@ class _Tugas5State extends State<Tugas5 > {
       ),
       backgroundColor: Colors.red
     ),
-     floatingActionButton: FloatingActionButton(
-        backgroundColor: Colors.red,
-        child: const Icon(Icons.camera),
-        onPressed: () {
-          setState(() {
-            counter ++;
-            
-          });
-        },
-     ),
+  
          body: SingleChildScrollView(
             padding: const EdgeInsets.all(20),
             child: Column(
@@ -116,44 +107,117 @@ class _Tugas5State extends State<Tugas5 > {
             ),
           ),
 
-          InkWell(
-            onTap: () {
-              print("Gambar diklik");
-            },
-            
-            child: Image.network("https://berbagikebaikan.or.id/wp-content/uploads/2024/05/sedekah-subuh.jpeg",height: 150,),
-            
-          ),
-          
-          GestureDetector(
-            onTap: () {
-              print("Disentuh Sekali");
-            },
-            onDoubleTap: () {
-              print("Disentuh dua kali");
-              },
-            onLongPress: () {
-              print("Tahan lama");
-            },  
-           child: Container(
-            color: Colors.redAccent,
-            padding: EdgeInsets.all(16),
-            child: Text("Donasi Sekarang"),
-           ), 
-          ),
+                InkWell(
+                  onTap: () {
+                    print("Gambar diklik");
+                  },
+                  child: ClipRRect(borderRadius: BorderRadius.circular(20),
+                  child: Image.network("https://berbagikebaikan.or.id/wp-content/uploads/2024/05/sedekah-subuh.jpeg",),
+                  ),
+                  ),
+                
+                SizedBox(height: 18),
 
+                GestureDetector(
+                  onTap: () {
+                    print("Disentuh Sekali");
+                  },
+                  onDoubleTap: () {
+                    print("Disentuh dua kali");
+                    },
+                  onLongPress: () {
+                    print("Tahan lama");
+                  },  
+                child: Container(
+                  color: Colors.redAccent,
+                  padding: EdgeInsets.all(16),
+                  child: Text("Donasi Sekarang"),
+                ), 
+                ),
+                SizedBox(height: 18),
+        
             Container(
-                padding: const EdgeInsets.all(20),
-                color: Colors.orange.shade200,
-                child: Text(
-                  "Counter: $counter",
+            margin: const EdgeInsets.only(bottom: 15),
+              padding: const EdgeInsets.symmetric(vertical: 8),
+              decoration: BoxDecoration(
+              color: Colors.white,
+              borderRadius: BorderRadius.circular(12),
+              boxShadow: [
+                BoxShadow(
+                  color: Colors.black12,
+                  blurRadius: 5,
+                  offset: Offset(0, 3),
+                ),
+              ],
+            ),
+            child: Column(
+              children: [
+
+                Text(
+                  "Total Donasi Terkumpul:",
                   style: const TextStyle(
-                    fontSize: 18,
+                    fontSize: 16,
+                  ),
+                ),
+
+                const SizedBox(height: 8),
+
+                Text(
+                  "Rp ${counter.toString()}",
+                  style: const TextStyle(
+                    fontSize: 20,
                     fontWeight: FontWeight.bold,
                   ),
                 ),
-              ),
+
+                const SizedBox(height: 15),
+
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+
+                    // Kurang
+                    
+                    ElevatedButton(
+                      onPressed: () {
+                        setState(() {
+                          if (counter >= 50000) {
+                            counter -= 50000;
+                          }
+                        });
+                      },
+                      style: ElevatedButton.styleFrom(
+                        backgroundColor: Colors.red,
+                      ),
+                      child: const Text("- 50.000"),
+                    ),
+
+                    const SizedBox(width: 15),
+
+                    // Tambah
+
+                    ElevatedButton(
+                      onPressed: () {
+                        setState(() {
+                          counter += 50000;
+                        });
+                      },
+                      style: ElevatedButton.styleFrom(
+                        backgroundColor: Colors.green,
+                      ),
+                      child: const Text("+ 50.000"),
+                    ),
+
+                  ],
+                ),
+              ],
+            ),
+          ),
+         
+         
+    
         ]
+    
 
 
       ),
